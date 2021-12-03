acd547979786cc7996c8ebc34555132505f3fac2
PaoloPascucci <pascuccip9@gmail.com>
2021-12-03 14:50:21 +0100

    Add logo and array with structure

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+$faq = [
+    [
+        'Domanda' => 'Come state implementando la recente decisione della Corte di giustizia dell Unione europea (CGUE) relativa al diritto all oblio?'
+        'Risposta' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde aspernatur praesentium porro? Ex error fugiat dolores temporibus quam placeat corporis nostrum voluptatibus consectetur, perspiciatis natus praesentium adipisci exercitationem aperiam commodi?'
+    ],
+    [
+        'Domanda' => 'Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?'
+        'Risposta' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde aspernatur praesentium porro? Ex error fugiat dolores temporibus quam placeat corporis nostrum voluptatibus consectetur, perspiciatis natus praesentium adipisci exercitationem aperiam commodi?'
+    ],
+    [
+        'Domanda' => 'Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?'
+        'Risposta' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde aspernatur praesentium porro? Ex error fugiat dolores temporibus quam placeat corporis nostrum voluptatibus consectetur, perspiciatis natus praesentium adipisci exercitationem aperiam commodi?'
+    ],
+    [
+        'Domanda' => 'Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?'
+        'Risposta' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde aspernatur praesentium porro? Ex error fugiat dolores temporibus quam placeat corporis nostrum voluptatibus consectetur, perspiciatis natus praesentium adipisci exercitationem aperiam commodi?'
+    ],
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google faq</title>
+    <style>
+        img{
+            vertical-align:middle;
+        }
+        span{
+            vertical-align:middle;
+            font-size:25px;
+        }
+        .flex{
+            display:flex;
+            flex-direction:row
+        }
+        li{
+            list-style:none;
+            margin:1rem;
+        }
+    </style>
+</head>
+<body>
+    <header>
+<img src="./img/Google-Logo.png" alt="Google" style="height:100px;"><span>Privacy e Termini</span>
+<ul class="flex">
+    <li>Introduzione</li>
+    <li>Norme sulle privacy</li>
+    <li>Termini di servizio</li>
+    <li>Tecnologie</li>
+    <li>Domande Frequenti</li>
+</ul>
+    </header>
+    <hr>
+    <main>
+        
+        <section>
+
+        </section>
+    </main>
+</body>
+</html>
